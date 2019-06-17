8c2a8fbe54c238edccb5553f04cfd19c9d8835be
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-06-17 03:19:23 -0700

    added fetch and finished form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer /> 
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    this.setState({isLoading: false});
+  }
+
+  render() {
+      return(
+        <div>
+          {this.state.isLoading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <div>
+              <Header />
+              <Main />
+              <Footer />
+            </div>
+          )}
+        </div>
+      )
+  }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,6 @@ import '../css/Main.css';
 import Form from './Form';
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-    componentDidMount() {
-
-    }
-
     render() {
         return (
             <section className="main">
