3abd161159c1e4f3f5046429ee18cffd8d81c1f3
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-06-26 23:54:18 -0700

    converted header to class for hamburger menu and added comments to main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/Header.css';
 
-function Header() {
-    return (
-        <header>
-            <div className="header-container">
-                <h1><i className="fas fa-beer" style={{color: 'red', fontSize: '25px'}}></i> Dakota's Brewery Directory</h1>
-                <nav>
-                    <ul>
-                        <li className="link"><a href="">Home</a></li>
-                        <li className="link"><a href="">About</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="header-container">
+                    <h1><i className="fas fa-beer" style={{color: 'red', fontSize: '25px'}}></i> Dakota's Brewery Directory</h1>
+                    <nav>
+                        <ul>
+                            <li className="link"><a href="">Home</a></li>
+                            <li className="link"><a href="">About</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -25,7 +25,7 @@ class Main extends Component {
             });
     }
 
-    // On submit of filter form, make another HTTP request
+    // On submit of filter form, make another HTTP request with filters
     handleSubmit(event) {
 
     }
@@ -33,74 +33,74 @@ class Main extends Component {
     render() {
         return (
             <div className="main-container">
-                    <h1>On the Pursuit of Hoppiness</h1>
-                    <p>Your guide to the best breweries in the United States</p>
-                    <div>
-                        {/* Start of filter form */}
-                        <form>
-                            <input type="text" name="" placeholder="Brewery Name"></input>
-                            {/* List of states alphabetized */}
-                            <select name="state">
-                                <option value="">Select a state</option>
-                                <option value="AL">Alabama</option>
-                                <option value="AK">Alaska</option>
-                                <option value="AZ">Arizona</option>
-                                <option value="AR">Arkansas</option>
-                                <option value="CA">California</option>
-                                <option value="CO">Colorado</option>
-                                <option value="CT">Connecticut</option>
-                                <option value="DE">Delaware</option>
-                                <option value="DC">District Of Columbia</option>
-                                <option value="FL">Florida</option>
-                                <option value="GA">Georgia</option>
-                                <option value="HI">Hawaii</option>
-                                <option value="ID">Idaho</option>
-                                <option value="IL">Illinois</option>
-                                <option value="IN">Indiana</option>
-                                <option value="IA">Iowa</option>
-                                <option value="KS">Kansas</option>
-                                <option value="KY">Kentucky</option>
-                                <option value="LA">Louisiana</option>
-                                <option value="ME">Maine</option>
-                                <option value="MD">Maryland</option>
-                                <option value="MA">Massachusetts</option>
-                                <option value="MI">Michigan</option>
-                                <option value="MN">Minnesota</option>
-                                <option value="MS">Mississippi</option>
-                                <option value="MO">Missouri</option>
-                                <option value="MT">Montana</option>
-                                <option value="NE">Nebraska</option>
-                                <option value="NV">Nevada</option>
-                                <option value="NH">New Hampshire</option>
-                                <option value="NJ">New Jersey</option>
-                                <option value="NM">New Mexico</option>
-                                <option value="NY">New York</option>
-                                <option value="NC">North Carolina</option>
-                                <option value="ND">North Dakota</option>
-                                <option value="OH">Ohio</option>
-                                <option value="OK">Oklahoma</option>
-                                <option value="OR">Oregon</option>
-                                <option value="PA">Pennsylvania</option>
-                                <option value="RI">Rhode Island</option>
-                                <option value="SC">South Carolina</option>
-                                <option value="SD">South Dakota</option>
-                                <option value="TN">Tennessee</option>
-                                <option value="TX">Texas</option>
-                                <option value="UT">Utah</option>
-                                <option value="VT">Vermont</option>
-                                <option value="VA">Virginia</option>
-                                <option value="WA">Washington</option>
-                                <option value="WV">West Virginia</option>
-                                <option value="WI">Wisconsin</option>
-                                <option value="WY">Wyoming</option>
-                            </select>
-                            <select>
-                                <option value="">Select a city</option>    
-                            </select>
-                            <button type="submit">Search</button>		
-                        </form>
-                        {/* End of filter form */}
-                    </div>
+                <h1>On the Pursuit of Hoppiness</h1>
+                <p>Your guide to the best breweries in the United States</p>
+                <div>
+                    {/* Start of filter form */}
+                    <form>
+                        <input type="text" name="" placeholder="Brewery Name"></input>
+                        {/* List of states alphabetized */}
+                        <select name="state">
+                            <option value="">Select a state</option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
+                        <select>
+                            <option value="">Select a city</option>    
+                        </select>
+                        <button type="submit">Search</button>		
+                    </form>
+                    {/* End of filter form */}
+                </div>
             </div>
         )
     }
