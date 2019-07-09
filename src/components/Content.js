84a041b3ed0f3b59bc5b0186d2afa388e82f9426
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-07-09 03:50:32 -0700

    added styling to card displays

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,15 +8,25 @@ class Content extends Component {
                 <h1>United States Craft Breweries</h1>
                 <div className="contentContainer">
                     {this.props.breweries.map((item, index) => {
+
+                        if (number !== "") {
+                            var number = item.phone;
+                            var newNum = "(" + number.substring(0, 3) + ") " + number.substring(3, 6) + "-" + number.substring(6, 10);
+                        }
+
                         return (
                             <div className="card">
-                                <h1>{item.name}</h1>
-                                <h3>Address</h3>
-                                <p>{item.street}</p>
-                                <p>{item.city}, {item.state} {item.postal_code}</p>
-                                <h3>Phone Number</h3>
-                                <p>{item.phone}</p>
-                                <button><a href={item.website_url} target="_blank">Website</a></button>
+                                <div id="header-container">
+                                    <h1>{item.name}</h1>
+                                </div>
+                                <div id="card-container">
+                                    <h3>Address</h3>
+                                    <p>{item.street}</p>
+                                    <p>{item.city}, {item.state} {item.postal_code}</p>
+                                    <h3>Phone Number</h3>
+                                    <p>{newNum}</p>
+                                    <button><a href={item.website_url} target="_blank">Website</a></button>
+                                </div>
                             </div>
                         )
                     })}
