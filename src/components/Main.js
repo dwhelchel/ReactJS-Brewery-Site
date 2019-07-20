743a6f2d3599e4db4ebc82e9efa62f0700836309
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-07-20 00:45:57 -0700

    added routing and fixed some mobile views

--- src/components/Main.js
+++ src/components/Main.js
@@ -84,73 +84,77 @@ class Main extends Component {
         return (
             <div>
                 <div className="main-container">
-                    <h1>On the Pursuit of Hoppiness</h1>
-                    <p>Your guide to the best breweries in the United States</p>
-                    <div>
-                        {/* Start of filter form */}
-                        <form>
-                            <input type="text" name="name" value={this.state.name} placeholder="Brewery Name" onChange={this.handleChange}></input>
-                            {/* List of states alphabetized */}
-                            <select name="state" value={this.state.state} onChange={this.handleChange}>
-                                <option value="">Select a state</option>
-                                <option value="Alabama">Alabama</option>
-                                <option value="Alaska">Alaska</option>
-                                <option value="Arizona">Arizona</option>
-                                <option value="Arkansas">Arkansas</option>
-                                <option value="California">California</option>
-                                <option value="Colorado">Colorado</option>
-                                <option value="Connecticut">Connecticut</option>
-                                <option value="Delaware">Delaware</option>
-                                <option value="District_Of_Columbia">District Of Columbia</option>
-                                <option value="Florida">Florida</option>
-                                <option value="Georgia">Georgia</option>
-                                <option value="Hawaii">Hawaii</option>
-                                <option value="Idaho">Idaho</option>
-                                <option value="Illinois">Illinois</option>
-                                <option value="Indiana">Indiana</option>
-                                <option value="Iowa">Iowa</option>
-                                <option value="Kansas">Kansas</option>
-                                <option value="Kentucky">Kentucky</option>
-                                <option value="Louisiana">Louisiana</option>
-                                <option value="Maine">Maine</option>
-                                <option value="Maryland">Maryland</option>
-                                <option value="Massachusetts">Massachusetts</option>
-                                <option value="Michigan">Michigan</option>
-                                <option value="Minnesota">Minnesota</option>
-                                <option value="Mississippi">Mississippi</option>
-                                <option value="Missouri">Missouri</option>
-                                <option value="Montana">Montana</option>
-                                <option value="Nebraska">Nebraska</option>
-                                <option value="Nevada">Nevada</option>
-                                <option value="New_Hampshire">New Hampshire</option>
-                                <option value="New_Jersey">New Jersey</option>
-                                <option value="New_Mexico">New Mexico</option>
-                                <option value="New_York">New York</option>
-                                <option value="North_Carolina">North Carolina</option>
-                                <option value="North_Dakota">North Dakota</option>
-                                <option value="Ohio">Ohio</option>
-                                <option value="Oklahoma">Oklahoma</option>
-                                <option value="Oregon">Oregon</option>
-                                <option value="Pennsylvania">Pennsylvania</option>
-                                <option value="Rhode_Island">Rhode Island</option>
-                                <option value="South_Carolina">South Carolina</option>
-                                <option value="South_Dakota">South Dakota</option>
-                                <option value="Tennessee">Tennessee</option>
-                                <option value="Texas">Texas</option>
-                                <option value="Utah">Utah</option>
-                                <option value="Vermont">Vermont</option>
-                                <option value="Virginia">Virginia</option>
-                                <option value="Washington">Washington</option>
-                                <option value="West_Virginia">West Virginia</option>
-                                <option value="Wisconsin">Wisconsin</option>
-                                <option value="Wyoming">Wyoming</option>
-                            </select>
-                            <select>
-                                <option value="">Select a city</option>    
-                            </select>
-                            <button type="button" onClick={this.handleClick}>Search</button>		
-                        </form>
-                        {/* End of filter form */}
+                    <div id="showcase-container">
+                        <div id="title-container">
+                            <h1>On the Pursuit of Hoppiness</h1>
+                            <p>Your guide to the best breweries in the United States</p>
+                        </div>
+                        <div id="form-container">
+                            {/* Start of filter form */}
+                            <form>
+                                <input type="text" name="name" value={this.state.name} placeholder="Brewery Name" onChange={this.handleChange}></input>
+                                {/* List of states alphabetized */}
+                                <select name="state" value={this.state.state} onChange={this.handleChange}>
+                                    <option value="">Select a state</option>
+                                    <option value="Alabama">Alabama</option>
+                                    <option value="Alaska">Alaska</option>
+                                    <option value="Arizona">Arizona</option>
+                                    <option value="Arkansas">Arkansas</option>
+                                    <option value="California">California</option>
+                                    <option value="Colorado">Colorado</option>
+                                    <option value="Connecticut">Connecticut</option>
+                                    <option value="Delaware">Delaware</option>
+                                    <option value="District_Of_Columbia">District Of Columbia</option>
+                                    <option value="Florida">Florida</option>
+                                    <option value="Georgia">Georgia</option>
+                                    <option value="Hawaii">Hawaii</option>
+                                    <option value="Idaho">Idaho</option>
+                                    <option value="Illinois">Illinois</option>
+                                    <option value="Indiana">Indiana</option>
+                                    <option value="Iowa">Iowa</option>
+                                    <option value="Kansas">Kansas</option>
+                                    <option value="Kentucky">Kentucky</option>
+                                    <option value="Louisiana">Louisiana</option>
+                                    <option value="Maine">Maine</option>
+                                    <option value="Maryland">Maryland</option>
+                                    <option value="Massachusetts">Massachusetts</option>
+                                    <option value="Michigan">Michigan</option>
+                                    <option value="Minnesota">Minnesota</option>
+                                    <option value="Mississippi">Mississippi</option>
+                                    <option value="Missouri">Missouri</option>
+                                    <option value="Montana">Montana</option>
+                                    <option value="Nebraska">Nebraska</option>
+                                    <option value="Nevada">Nevada</option>
+                                    <option value="New_Hampshire">New Hampshire</option>
+                                    <option value="New_Jersey">New Jersey</option>
+                                    <option value="New_Mexico">New Mexico</option>
+                                    <option value="New_York">New York</option>
+                                    <option value="North_Carolina">North Carolina</option>
+                                    <option value="North_Dakota">North Dakota</option>
+                                    <option value="Ohio">Ohio</option>
+                                    <option value="Oklahoma">Oklahoma</option>
+                                    <option value="Oregon">Oregon</option>
+                                    <option value="Pennsylvania">Pennsylvania</option>
+                                    <option value="Rhode_Island">Rhode Island</option>
+                                    <option value="South_Carolina">South Carolina</option>
+                                    <option value="South_Dakota">South Dakota</option>
+                                    <option value="Tennessee">Tennessee</option>
+                                    <option value="Texas">Texas</option>
+                                    <option value="Utah">Utah</option>
+                                    <option value="Vermont">Vermont</option>
+                                    <option value="Virginia">Virginia</option>
+                                    <option value="Washington">Washington</option>
+                                    <option value="West_Virginia">West Virginia</option>
+                                    <option value="Wisconsin">Wisconsin</option>
+                                    <option value="Wyoming">Wyoming</option>
+                                </select>
+                                <select>
+                                    <option value="">Select a city</option>    
+                                </select>
+                                <button type="button" onClick={this.handleClick}>Search</button>		
+                            </form>
+                            {/* End of filter form */}
+                        </div>
                     </div>
                 </div>
                 <div>
