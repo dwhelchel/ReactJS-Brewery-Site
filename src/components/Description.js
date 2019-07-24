ad485d81db4b9444317a2086cda6590dadd63865
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-07-24 01:53:18 -0700

    began work on about page

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../css/Description.css';
 
 function Description() {
     return(
-        <div>
+        <div id="backdrop-container">
             <div id="description-container">
                 <p>Welcome to Dakota's Brewery Directory! This web application allows the user to browse a list of
                     craft breweries in the United States. Although not all breweries in the United States are listed
