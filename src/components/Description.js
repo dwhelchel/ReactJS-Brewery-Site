bf24e538b8c6cf6643c99cb40c75708722703f99
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-07-30 20:46:45 -0700

    added more descriptions and link to the Open Brewery DB

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,9 +5,14 @@ function Description() {
     return(
         <div id="backdrop-container">
             <div id="description-container">
-                <p>Welcome to Dakota's Brewery Directory! This web application allows the user to browse a list of
-                    craft breweries in the United States. Although not all breweries in the United States are listed
-                    here, the Open Brewery Database is currently in beta, and will be updated in the future.</p>
+                <div id="paragraph-container">
+                    <p>Welcome to Dakota's Brewery Directory! This web application allows the user to browse a list of
+                        craft breweries in the United States. Although not all breweries in the United States are listed
+                        here, the Open Brewery Database is currently in beta, and will be updated in the future.</p>
+                    <p>I am very passionate about the craft beer community, so I wanted to create an application where
+                        people like me could find and experience different craft breweries in each state. This project is powered
+                        by the <a style={{color: 'red'}} href="https://www.openbrewerydb.org/" target="_blank" rel="noopener noreferrer">Open Brewery DB</a>.</p>
+                </div>
             </div>
         </div>
     );
