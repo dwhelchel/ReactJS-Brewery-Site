8fc5b49900d88f5ae621a8fdefd13342e1394956
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-08-16 01:20:37 -0700

    added animation for list to ease-in on burger press

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,22 @@ import '../css/Header.css';
 class Header extends Component {
     render() {
 
-        
+        function navigationSlide() {
+            const nav = document.querySelector('.nav');
+            nav.classList.toggle('navigation-active');
+        }
 
         return (
             <header>
                 <div className="header-container">
                     <h1><i className="fas fa-beer" style={{color: 'red', fontSize: '20px'}}></i><span style={{color: 'red'}}> Dakota's</span> Brewery Directory</h1>
-                    <nav>
+                    <nav className="nav">
                         <ul>
                             <Link to="/"><li className="link">Home</li></Link>
                             <Link to="/about"><li className="link">About</li></Link>
                         </ul>
                     </nav>
-                    <div className="burger">
+                    <div className="burger" onClick={navigationSlide}>
                         <div className="line1"></div>
                         <div className="line2"></div>
                         <div className="line3"></div>
